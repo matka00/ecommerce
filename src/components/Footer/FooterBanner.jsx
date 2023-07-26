19bf28d76e973b262e4e1e6aab657c69b02883a6
matka00 <anita.skumat@gmail.com>
2023-07-26 16:39:05 +0200

    updated footer banner's responsiveness

--- src/components/Footer/FooterBanner.jsx
+++ src/components/Footer/FooterBanner.jsx
@@ -8,25 +8,23 @@ function FooterBanner({ footerBanner }) {
   console.log(footerBanner);
   return (
     <div className="footer-banner-cont">
-      <div className="footer-banner-desc">
-        <div className="left">
-          <p>{footerBanner.discount}</p>
-          <h3>{footerBanner.largeText1}</h3>
-          <p>{footerBanner.saleTime}</p>
-        </div>
-        <div className="right">
-          <p>{footerBanner.smallText}</p>
-          <h3>{footerBanner.midText}</h3>
-          <p>{footerBanner.description}</p>
-          <Link to={`/product/${footerBanner.product}`}>
-            <button type="button">{footerBanner.buttonText}</button>
-          </Link>
-        </div>
-        <img
-          src={urlFor(footerBanner.image)}
-          alt={footerBanner.product}
-          className="footer-banner-image"
-        />
+      <div className="footer-banner-left">
+        <p>{footerBanner.discount}</p>
+        <h3>{footerBanner.largeText1}</h3>
+        <p>{footerBanner.saleTime}</p>
+      </div>
+      <img
+        src={urlFor(footerBanner.image)}
+        alt={footerBanner.product}
+        className="footer-banner-image"
+      />
+      <div className="footer-banner-right">
+        <p>{footerBanner.smallText}</p>
+        <h3>{footerBanner.midText}</h3>
+        <p>{footerBanner.description}</p>
+        <Link to={`/product/${footerBanner.product}`}>
+          <button type="button">{footerBanner.buttonText}</button>
+        </Link>
       </div>
     </div>
   );
