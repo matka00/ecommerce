974e3248952b43aa5ecdbd7cd68a7b61ba7fb155
matka00 <anita.skumat@gmail.com>
2023-08-03 20:24:21 +0200

    added nav links and hamburger menu for mobile view

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,30 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+
+import { GiWool } from "react-icons/gi";
 import { AiOutlineShopping } from "react-icons/ai";
+import { FiMenu } from "react-icons/fi";
+import { IoCloseOutline } from "react-icons/io5";
 
 import Cart from "../cart/Cart";
 import { useStateContext } from "../../context/StateContext";
 
 function Navbar() {
   const { showCart, setShowCart, totalQuantities } = useStateContext();
+  const [click, setClick] = useState(false);
 
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
+  console.log(click);
   return (
     <>
       <div className="navbar-cont">
-        <Link to="/" className="navbar-logo">
-          Handmade Shop
+        <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
+          <GiWool />
+          hms
         </Link>
-        <Link to="/products">Products</Link>
-        <button
-          type="button"
-          className="cart-icon"
-          onClick={() => setShowCart(true)}
-        >
-          <AiOutlineShopping />
-          <span className="cart-item-qty">{totalQuantities}</span>
-        </button>
+        <div className="nav-right-wrapper">
+          <div className="menu-icon-cont" onClick={handleClick}>
+            <span className="menu-icon">
+              <FiMenu className={click ? "icon-hide" : "icon-show"} />
+            </span>
+            <span className="menu-icon">
+              <IoCloseOutline className={click ? "icon-show" : "icon-hide"} />
+            </span>
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <NavLink to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/products"
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                Products
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/
+            "
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                About Us
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink
+                to="/
+            "
+                className="nav-links"
+                onClick={closeMobileMenu}
+              >
+                FAQ
+              </NavLink>
+            </li>
+          </ul>
 
+          <div className="cart-icon-cont">
+            <button
+              type="button"
+              className="cart-icon"
+              onClick={() => setShowCart(true)}
+            >
+              <AiOutlineShopping />
+              <span className="cart-item-qty">{totalQuantities}</span>
+            </button>
+          </div>
+        </div>
         {showCart && <Cart />}
       </div>
     </>
